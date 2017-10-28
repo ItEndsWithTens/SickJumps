7bee3f18eebe449e5d25ec011fe3d48d8391094b
Rob Martens <robert.martens@gmail.com>
2017-10-28 16:53:14 -0400

    Switch Catch test cases from REQUIRE to CHECK
    Allows execution to continue past failures. This can be useful when
    various different failed checks might, in concert, point to a specific
    section of code as the culprit.

--- test/src/frames.cpp
+++ test/src/frames.cpp
@@ -12,47 +12,47 @@ extern IScriptEnvironment* env;
 
 TEST_CASE("Ramp input frame count matches output frame count when multipliers are both 1.0")
 {
-	REQUIRE(CalculateRampInputFrames(0, 0, 1.0, 1.0, SickJumps::MODE_LINEAR) == 0);
-	REQUIRE(CalculateRampInputFrames(0, 10, 1.0, 1.0, SickJumps::MODE_LINEAR) == 10);
-	REQUIRE(CalculateRampInputFrames(0, 100, 1.0, 1.0, SickJumps::MODE_LINEAR) == 100);
-	REQUIRE(CalculateRampInputFrames(0, 1000, 1.0, 1.0, SickJumps::MODE_LINEAR) == 1000);
+	CHECK(CalculateRampInputFrames(0, 0, 1.0, 1.0, SickJumps::MODE_LINEAR) == 0);
+	CHECK(CalculateRampInputFrames(0, 10, 1.0, 1.0, SickJumps::MODE_LINEAR) == 10);
+	CHECK(CalculateRampInputFrames(0, 100, 1.0, 1.0, SickJumps::MODE_LINEAR) == 100);
+	CHECK(CalculateRampInputFrames(0, 1000, 1.0, 1.0, SickJumps::MODE_LINEAR) == 1000);
 
-	REQUIRE(CalculateRampInputFrames(0, 667, 1.0, 1.0, SickJumps::MODE_LINEAR) == 667);
-	REQUIRE(CalculateRampInputFrames(0, 5353, 1.0, 1.0, SickJumps::MODE_LINEAR) == 5353);
-	REQUIRE(CalculateRampInputFrames(0, 9999, 1.0, 1.0, SickJumps::MODE_LINEAR) == 9999);
+	CHECK(CalculateRampInputFrames(0, 667, 1.0, 1.0, SickJumps::MODE_LINEAR) == 667);
+	CHECK(CalculateRampInputFrames(0, 5353, 1.0, 1.0, SickJumps::MODE_LINEAR) == 5353);
+	CHECK(CalculateRampInputFrames(0, 9999, 1.0, 1.0, SickJumps::MODE_LINEAR) == 9999);
 }
 
 TEST_CASE("Frame numbers are what they should be when multipliers are both 1.0")
 {
 	SickJumpsCore c = SickJumpsCore(1000, 100, 500, 60.0, 2.0, 2.0, 1.0, 1.0, 800, SickJumps::MODE_LINEAR);
 
-	REQUIRE(c.originalFrameCount == c.adjustedFrameCount);
-	REQUIRE(c.originalSampleCount == c.adjustedSampleCount);
+	CHECK(c.originalFrameCount == c.adjustedFrameCount);
+	CHECK(c.originalSampleCount == c.adjustedSampleCount);
 
-	REQUIRE(c.rampUpFirstInputFrame == 100);
-	REQUIRE(c.rampDownLastInputFrame == 500);
+	CHECK(c.rampUpFirstInputFrame == 100);
+	CHECK(c.rampDownLastInputFrame == 500);
 	
-	REQUIRE(c.rampUpLastInputFrame == 219);
-	REQUIRE(c.rampDownFirstInputFrame == 381);
+	CHECK(c.rampUpLastInputFrame == 219);
+	CHECK(c.rampDownFirstInputFrame == 381);
 }
 
 TEST_CASE()
 {
 	SickJumpsCore c = SickJumpsCore(100000, 10000, 90000, 60.0, 2.0, 2.0, 1.0, 8.0, 800, SickJumps::MODE_LINEAR);
 
-	REQUIRE(c.rampUpFirstInputFrame == 10000);
-	REQUIRE(c.rampDownLastInputFrame == 90000);
+	CHECK(c.rampUpFirstInputFrame == 10000);
+	CHECK(c.rampDownLastInputFrame == 90000);
 
 	double average = (c.startMultiplier + c.fullMultiplier) / 2.0;
 	
 	int rampUpDiff = c.rampUpLastOutputFrame - c.rampUpFirstOutputFrame;
 	int rampUpLastInputFrame = c.rampUpFirstInputFrame + static_cast<int>(std::round(rampUpDiff * average));
-	REQUIRE(c.rampUpLastInputFrame == rampUpLastInputFrame);
+	CHECK(c.rampUpLastInputFrame == rampUpLastInputFrame);
 
 	int rampDownDiff = c.rampDownLastOutputFrame - c.rampDownFirstOutputFrame;
 	int rampDownFirstInputFrame = c.rampDownLastInputFrame - static_cast<int>(std::round(rampDownDiff * average));
 	rampDownFirstInputFrame += static_cast<int>(std::round(c.fullMultiplier));
-	REQUIRE(c.rampDownFirstInputFrame == rampDownFirstInputFrame);
+	CHECK(c.rampDownFirstInputFrame == rampDownFirstInputFrame);
 }
 
 TEST_CASE("Ramps start and end on proper audio samples with a full multiplier of 1.0")
@@ -62,33 +62,33 @@ TEST_CASE("Ramps start and end on proper audio samples with a full multiplier of
 
 	SickJumpsCore c = SickJumpsCore(100000, startFrame, endFrame, 60.0, 2.0, 2.0, 1.0, 1.0, 800, SickJumps::MODE_LINEAR);
 
-	REQUIRE(c.rampUpFirstInputSample == c.audioSamplesPerFrame * startFrame);
-	REQUIRE(c.rampDownLastInputSample == (c.audioSamplesPerFrame * (endFrame + 1)) - 1);
+	CHECK(c.rampUpFirstInputSample == c.audioSamplesPerFrame * startFrame);
+	CHECK(c.rampDownLastInputSample == (c.audioSamplesPerFrame * (endFrame + 1)) - 1);
 
-	REQUIRE(c.GetAdjustedSampleNumber(0) == 0);
-	REQUIRE(c.GetAdjustedSampleNumber(startFrame * c.audioSamplesPerFrame) == startFrame * c.audioSamplesPerFrame);
+	CHECK(c.GetAdjustedSampleNumber(0) == 0);
+	CHECK(c.GetAdjustedSampleNumber(startFrame * c.audioSamplesPerFrame) == startFrame * c.audioSamplesPerFrame);
 }
 
 TEST_CASE("Ramps start and end on proper audio samples with a full multiplier of 2.0")
 {
 	SickJumpsCore c = SickJumpsCore(100000, 10000, 90000, 60.0, 2.0, 2.0, 1.0, 2.0, 800, SickJumps::MODE_LINEAR);
 
-	REQUIRE(c.rampUpFirstInputSample == c.audioSamplesPerFrame * 10000);
-	REQUIRE(c.rampDownLastInputSample == (c.audioSamplesPerFrame * 90001) - 1);
+	CHECK(c.rampUpFirstInputSample == c.audioSamplesPerFrame * 10000);
+	CHECK(c.rampDownLastInputSample == (c.audioSamplesPerFrame * 90001) - 1);
 }
 
 TEST_CASE("Ramps start and end on proper audio samples with a full multiplier of 4.0")
 {
 	SickJumpsCore c = SickJumpsCore(100000, 10000, 90000, 60.0, 2.0, 2.0, 1.0, 4.0, 800, SickJumps::MODE_LINEAR);
 
-	REQUIRE(c.rampUpFirstInputSample == c.audioSamplesPerFrame * 10000);
-	REQUIRE(c.rampDownLastInputSample == (c.audioSamplesPerFrame * 90001) - 1);
+	CHECK(c.rampUpFirstInputSample == c.audioSamplesPerFrame * 10000);
+	CHECK(c.rampDownLastInputSample == (c.audioSamplesPerFrame * 90001) - 1);
 }
 
 TEST_CASE("Ramps start and end on proper audio samples with a full multiplier of 8.0")
 {
 	SickJumpsCore c = SickJumpsCore(100000, 10000, 90000, 60.0, 2.0, 2.0, 1.0, 8.0, 800, SickJumps::MODE_LINEAR);
 
-	REQUIRE(c.rampUpFirstInputSample == c.audioSamplesPerFrame * 10000);
-	REQUIRE(c.rampDownLastInputSample == (c.audioSamplesPerFrame * 90001) - 1);
+	CHECK(c.rampUpFirstInputSample == c.audioSamplesPerFrame * 10000);
+	CHECK(c.rampDownLastInputSample == (c.audioSamplesPerFrame * 90001) - 1);
 }
